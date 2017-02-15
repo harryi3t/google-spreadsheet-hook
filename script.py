6e5d2885b2abe3dc2a9172516949e18132f2ddc9
Harendra Singh <66hari@gmail.com>
2017-02-15 13:52:09 +0530

    get rid of warning message

--- script.py
+++ script.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import urllib
 
 r = urllib.urlopen('http://www.practice.geeksforgeeks.org/user-profile.php?user=harendra%20Singh').read()
-soup = BeautifulSoup(r)
+soup = BeautifulSoup(r, 'lxml')
 tags = soup.select('#detail1 div.panel-body div div.col-sm-8 table tbody tr:nth-of-type(2) td:nth-of-type(2)')
 if len(tags) != 1:
   print "Something has changed in website structure. Rank not found"
